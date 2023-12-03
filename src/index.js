b16eecf84f094ad78546062bc60cf2281d8600be
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-03 18:33:49 +0100

    Add Routing and PersistGate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { store } from 'redux/store';
 import { Provider } from 'react-redux';
+import { store, persistor} from 'redux/store';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App';
 
 import './index.css';
@@ -10,7 +11,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );

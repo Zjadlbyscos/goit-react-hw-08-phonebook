df61d2994a497546fdad1e01fbf0fde5f901d280
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-03 22:34:36 +0100

    update store /add persist/ fix persistor bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { sliceContact } from './sliceContact';
-import { sliceFilter } from './sliceFilter';
+import { contactsReducer } from './sliceContact';
+import { filterReducer } from './sliceFilter';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { authReducer } from './User/userSlice';
+
+const persistConfig = {
+  key: 'token',
+  storage,
+  whiteList: ['token'],
+};
+
+const persistReducerUser = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: sliceContact.reducer,
-    filter: sliceFilter.reducer,
+    contacts: contactsReducer,
+    filter: filterReducer,
+    auth: persistReducerUser,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistorProvider = persistStore(store);

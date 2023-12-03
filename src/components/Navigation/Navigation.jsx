2cb472f9d9aa063f4d6bc0fee036357cbe27612f
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-03 22:36:47 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,51 +1,43 @@
-import InfoUser from 'components/InfoUser/InfoUser';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-// import { selectToken, selectUser } from 'redux/user/userSelect';
+
+import InfoUser from 'components/InfoUser/InfoUser';
+import { selectToken, selectUser } from 'redux/constants';
 
 export const Navigation = () => {
-//   const user = useSelector(selectUser);
-//   const token = useSelector(selectToken) ?? '';
-
+  const user = useSelector(selectUser);
+  const token = useSelector(selectToken) ?? '';
+  console.log(user);
   return (
     <>
       <nav>
         <div>
-          <Link to="/goit-react-hw-08-phonebook/contacts">
-            Phonebook
-          </Link>
-          {/* {!user && ( */}
+          <Link to="/goit-react-hw-08-phonebook/contacts">Phonebook</Link>
+          {!user && (
             <ul>
-              {/* {token && ( */}
+              {token && (
                 <li>
-                  <Link to="/contacts">
-                    Contacts
-                  </Link>
+                  <Link to="/contacts">Contacts</Link>
                 </li>
-              {/* )} */}
+              )}
               <li>
-                <Link to="/signup">
-                  Sign Up
-                </Link>
+                <Link to="/signup">Sign Up</Link>
               </li>
               <li>
-                <Link to="/login">
-                  Login
-                </Link>
+                <Link to="/login">Login</Link>
               </li>
             </ul>
-          {/* )} */}
+          )}
         </div>
         <InfoUser />
       </nav>
-      {/* <div>
+      <div>
         {user ? (
           <h1>Welcome {user.name} to your contacts</h1>
         ) : (
           <h1>Welcome guest, please login</h1>
         )}
-      </div> */}
+      </div>
     </>
   );
 };
-

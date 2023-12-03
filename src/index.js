f276b92066dd7ef5301f49f8c7a184ff27a420b9
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-03 22:36:27 +0100

    fix persistor - provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store, persistor} from 'redux/store';
+import { store, persistorProvider} from 'redux/store';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -11,13 +11,13 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} 
-      persistor={persistor}
-      > */}
+      <PersistGate loading={null} 
+      persistor={persistorProvider}
+      >
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );

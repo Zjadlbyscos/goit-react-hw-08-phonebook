49cfa95b5e354bab7577ca8bb92c132da8bd16f6
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-03 22:34:49 +0100

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -27,3 +27,32 @@ export const updContacts = async id => {
   const { data } = await backendAPI.patch(`/contacts/${id}`);
   return data;
 };
+
+export const token = {
+  set: token => {
+    backendAPI.defaults.headers.Authorization = `Bearer ${token}`;
+  },
+  unSet: token => {
+    backendAPI.defaults.headers.Authorization = '';
+  },
+};
+
+export const signUpUser = async credentials => {
+  const { data } = await backendAPI.post('users/signup', credentials);
+  return data;
+};
+
+export const loginUser = async credentials => {
+  const { data } = await backendAPI.post('users/login', credentials);
+  console.log(data);
+  return data;
+};
+
+export const logoutUser = async () => {
+  return backendAPI.post('users/logout');
+};
+
+export const currentUser = async () => {
+  const data = await backendAPI.get('users/current');
+  return data;
+};

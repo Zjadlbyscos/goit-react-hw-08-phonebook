e87edbf8abc296f98aff2c5b4cc41fc3bce48481
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-12-04 10:31:32 +0100

    Create InfoUser

--- src/components/InfoUser/InfoUser.jsx
+++ src/components/InfoUser/InfoUser.jsx
@@ -1,11 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutThunk } from 'redux/User/userThunk';
+import { selectUser } from 'redux/constants';
 
+import s from './InfoUser.module.css';
+const InfoUser = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-export const InfoUser = () => {
-
-    return (
-    <p>INFO USER</p>
-    );
+  const handleSubmit = () => {
+    dispatch(logoutThunk());
   };
-  
-  export default InfoUser;
-  
+
+  return (
+    <div className={s.container}>
+      {user && (
+        <div className={s.info}>
+          <h4>HI, {user.name}</h4>
+          <button className={s.btn} onClick={handleSubmit}>
+            Log Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InfoUser;
